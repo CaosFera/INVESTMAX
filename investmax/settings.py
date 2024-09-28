31370c66746c24fb5d133afa5cfcdf8cf25abf09
Braga <bragadevelopment@gmail.com>
2024-09-27 22:30:17 -0300

    Excluindo processador de contexto

--- investmax/settings.py
+++ investmax/settings.py
@@ -80,9 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'investments.context_processors.logomarca',
-                'users.context_processors.photo_profile',
+                'django.contrib.messages.context_processors.messages',                            
                 
             ],
         },
